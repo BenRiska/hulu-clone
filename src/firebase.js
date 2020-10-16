b793aaa9667b7991b1f5b7823e228e55e94453d7
bentaylor <ben10taylor10@hotmail.com>
2020-10-16 16:01:30 +0100

    done

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCH-7tMIUNdfoyNx_2wK4tKD1oZnm_gesk",
+    authDomain: "hulu-clone-a3507.firebaseapp.com",
+    databaseURL: "https://hulu-clone-a3507.firebaseio.com",
+    projectId: "hulu-clone-a3507",
+    storageBucket: "hulu-clone-a3507.appspot.com",
+    messagingSenderId: "334241515891",
+    appId: "1:334241515891:web:6c6074dc42ad0c87be9d06",
+    measurementId: "G-Y5XLHT6KV2"
+  }; 
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  
